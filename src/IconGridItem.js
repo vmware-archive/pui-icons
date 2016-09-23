b1747ccb6697729f8410192fe4006814ca429981
Matt Rothenberg <mattrothenberg@gmail.com>
2016-09-23 15:57:25 -0400

    aaand we still have problems on mobile. reverting

--- src/IconGridItem.js
+++ src/IconGridItem.js
@@ -7,7 +7,7 @@ class IconGridItem extends Component {
     return(
       <li key={this.props.index} className="icon-list-item">
         <div className="icon-list-item-content">
-          <InlineSVG src={this.props.path} className="icon-preview"/>
+          <img src={this.props.path} className="icon-preview"/>
           <pre className="icon-classname">{ this.props.name }</pre>
         </div>
       </li>
